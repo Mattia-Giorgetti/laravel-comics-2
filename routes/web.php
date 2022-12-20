63ee825f12bd80912b663dd406236eb5e4af2f3b
Mattia-Giorgetti <mattiagiorgetti37@gmail.com>
2022-12-20 17:37:09 +0100

    fix redirect home

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-// Route::get('/', function () {
-//     return view('home');
-// });
+Route::get('/', function () {
+    return redirect()->route('comics.index');
+});
 Route::resource('comics', ResourceController::class);
